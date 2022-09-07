6adc1603189f8ff0cf31929a72219cb4f24f1313
Aleksandr Marchenko <proshta@gmail.com>
2022-09-07 17:23:44 +0300

    added link to sharik.ua

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import style from "./../styles/Photo.module.css";
 import { FaPhotoVideo } from "react-icons/fa";
 
+import { TbBallon } from "react-icons/tb";
+
 
 const Photo = ({ artB }) => {
 
 
 
-	const photo = `https://sharik.ua/images/elements_big/${artB}_m1.jpg`
+	const photo = `https://sharik.ua/images/elements_big/${artB}_m1.jpg`;
+
+	const link = `https://sharik.ua/ua/search/?q=${artB}`
 
 
 	return (
 		<div className={style.photo}>
 
-			{artB.length == 9 ? <img src={photo} alt="artB" /> : <FaPhotoVideo />}
+			{artB.length == 9 ?
+				<a href={link} target="_blanked">
+					<img src={photo} alt="Здесь должно быть изображение артикула" ></img>
+				</a> :
+				<TbBallon />}
+
+
 
 		</div>
 	);
